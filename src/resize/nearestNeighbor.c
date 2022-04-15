4c32ca3e0baff459ce7ceec7a9774b064bf5d1bc
Hrvoje Cosic <62515219+HrvojeCosic@users.noreply.github.com>
2022-04-15 18:28:40 +0200

    add: resize by nearest neighbor for downscaling
    needs adjustments to work for upscaling too

--- src/resize/nearestNeighbor.c
+++ src/resize/nearestNeighbor.c
@@ -1,15 +1,39 @@
 #include "nearestNeighbor.h"
+#include "../Image.h"
 
-void resize(char* filename, IMAGE props) {
+void resize(char* filename, IMAGE newProps) {
     IMAGE originalImg = readImg(filename);
-    fixAspectRatio(originalImg, &props.width, &props.height);
+    fixAspectRatio(originalImg, &newProps.width, &newProps.height);
+
+    newProps.size = newProps.width * newProps.height * originalImg.channelNumber;
+    if (newProps.size <= 0) {
+        printf("Target image properties are invalid\n");
+        exit(1);    
+    }
+    newProps.data = malloc(newProps.size * sizeof(uint8_t));
+
+    double xRatio = originalImg.width / newProps.width;
+    double yRatio = originalImg.height / newProps.height;
+    int ch = originalImg.channelNumber;
+    for (int y=0; y<newProps.height; y++) {
+        for (int x=0; x<newProps.width; x++) {
+            int newPos = (x + y * newProps.width) * ch;
+            int orgPos = ( (x * xRatio) + (y * originalImg.width * yRatio) ) * ch;
+
+            for (int i=0; i<ch; i++) 
+                newProps.data[newPos + i] = originalImg.data[orgPos + i];
+        }
+    }
+
+    writeToImg("./processedImg.jpg", newProps);
+    printf("Image resized successfuly.");
 }
 
 void fixAspectRatio(IMAGE image, int* wProp, int* hProp) {
     float aspectRatio = (float)image.width / image.height;
     if (*wProp != 0) {
-        *hProp = (*wProp)/aspectRatio;
+        *hProp = (*wProp) / aspectRatio;
     } else if (*hProp != 0) {
-        *wProp = (*hProp)*aspectRatio;
+        *wProp = (*hProp) * aspectRatio;
     }
 }
