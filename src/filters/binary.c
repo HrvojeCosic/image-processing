8e0aa9dbb4f4e25e73c6018906795f3342e28d91
Hrvoje Cosic <62515219+HrvojeCosic@users.noreply.github.com>
2022-04-16 23:17:31 +0200

    add: image's pixel histogram computation

--- src/filters/binary.c
+++ src/filters/binary.c
@@ -1,16 +1,19 @@
 #include "filters.h"
+#include "filters_internal.h"
 
 void applyBinary(char* filename) {
     IMAGE img = readImg(filename);
 
     if (img.channelNumber >= 3) {
         applyGrayscale(filename);
-        img = readImg("./exampleImg.jpg");
+        img = readImg("./processedImg.jpg");
     }
+
+    int threshold = computeThreshold(img);
 
     for (int i=0; i<img.size; i+=img.channelNumber) {
         int greyLvl = *(img.data+i);
-        int finalPixel = (greyLvl >= 0 && greyLvl < 50) ? 0 : 255;
+        int finalPixel = greyLvl < threshold ? 0 : 255;
 
         for (int j=i; j < i+img.channelNumber; j++)
             *(img.data+j) = finalPixel;
@@ -19,4 +22,27 @@ void applyBinary(char* filename) {
     writeToImg("./processedImg.jpg", img);
     printf("Binary filter applied successfuly.\n");
     free(img.data);
+}
+
+int computeThreshold(IMAGE img) {
+    int* histogram = getPixelHistogram(img);
+    return 50; // TODO: naravno ne ovo
+}
+
+int* getPixelHistogram (IMAGE img) {
+    static int histogram[256];
+    const int maxPixelIntensity = 255;
+    for (int i=0; i<=maxPixelIntensity; i++)
+        histogram[i] = 0;
+
+    for (int i=0; i<img.size; i+=img.channelNumber) {
+        int channelAvg = 0;
+        int ch = img.channelNumber;
+        while(ch != 0) channelAvg += *(img.data + i + (--ch));
+        channelAvg /= img.channelNumber;
+
+        histogram[channelAvg]++;
+    }
+
+    return histogram;
 }
---
+++ src/filters/filters_internal.h
@@ -0,0 +1,9 @@
+#ifndef FILTERS_INTERNAL_H
+#define FILTERS_INTERNAL_H
+
+#include "../Image.h"
+
+int computeThreshold(IMAGE);
+int* getPixelHistogram(IMAGE);
+
+#endif
