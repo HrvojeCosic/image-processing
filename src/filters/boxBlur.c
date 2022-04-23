5183c6e6c5f08e55e112ccc0cea1ff6b4957bfe5
Hrvoje Cosic <62515219+HrvojeCosic@users.noreply.github.com>
2022-04-23 10:59:32 +0200

    add: box blur using fixed 3x3 kernel
    +removed redundant sum function

--- src/filters/boxBlur.c
+++ src/filters/boxBlur.c
@@ -6,10 +6,20 @@ void applyBoxBlur(char* filename) {
     img = readImg(filename);
     IMAGE newImg = readImg(filename);
 
-    for (int y=0; y<img.height*img.channelNumber; y++) {
+    for (int y=0; y<img.height * img.channelNumber; y++) {
         for (int x=0; x<img.width; x++) {
-            int newPixelValue = getNeighborSum((y * img.width) + x) / 9;
-            newImg.data[y*img.width + x] = newPixelValue;
+            int neighborsSum = 0;
+            neighborsSum += *(img.data + (img.width * y) + x);
+            neighborsSum += *(img.data + (img.width * y) + x + img.channelNumber);
+            neighborsSum += *(img.data + (img.width * y) + x - img.channelNumber);
+            neighborsSum += *(img.data + (img.width * y + img.channelNumber) + x);
+            neighborsSum += *(img.data + (img.width * y - img.channelNumber) + x);
+            neighborsSum += *(img.data + (img.width * y - img.channelNumber) + x + img.channelNumber);
+            neighborsSum += *(img.data + (img.width * y + img.channelNumber) + x - img.channelNumber);
+            neighborsSum += *(img.data + (img.width * y - img.channelNumber) + x - img.channelNumber);
+            neighborsSum += *(img.data + (img.width * y + img.channelNumber) + x + img.channelNumber);
+
+            newImg.data[y*img.width + x] = neighborsSum / 9;
         }
     }
 
@@ -18,29 +28,4 @@ void applyBoxBlur(char* filename) {
     free(newImg.data);
     free(img.data);
     return;
-}
-
-double getNeighborSum(int centralPixel) {
-    int neighborTop = centralPixel - img.width;
-    int neighborLeft = centralPixel - img.channelNumber;
-    int neighborTopLeft = neighborLeft - img.width;
-    int neighborBottomLeft = neighborLeft + img.width;
-    int neighborRight = centralPixel + img.channelNumber;
-    int neighborTopRight = neighborRight - img.width;
-    int neighborBottomRight = neighborRight + img.width;
-    int neighborBottom = centralPixel + img.width;
-
-    int neighbors[] = {
-        neighborTopLeft, neighborTop, neighborTopRight,
-        neighborLeft, centralPixel, neighborRight,
-        neighborBottomLeft, neighborBottom, neighborBottomRight
-        };
-    int neighborLen = sizeof(neighbors) / sizeof(neighbors[0]);
-    
-    double neighborSum = 0;
-    for (int i=0; i<neighborLen; i++) {
-        if (neighbors[i] < 0 || neighbors[i] > img.size) continue;
-        neighborSum += *(img.data+neighbors[i]);
-    }
-    return neighborSum;
 }
