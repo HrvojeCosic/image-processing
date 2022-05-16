26c35c40d5112e39c71b209082c5222c80d5f59b
Hrvoje Cosic <62515219+HrvojeCosic@users.noreply.github.com>
2022-05-16 16:05:16 +0200

    add: safety for pixel sorting large images
    + move pixel quicksort to internal .c file

--- src/filters/filters_internal.c
+++ src/filters/filters_internal.c
@@ -70,3 +70,42 @@ int getVerticalGradient (IMAGE img, int y, int x, int kernelY[3][3]) {
 
     return gradientY;
 }
+
+void quickSortPixels(uint8_t* arr, int l, int r, int chN) {
+    if (l >= r) return;
+    int i=l-chN, j=0, chTemp;
+    for (j=l; j<r; j+=chN) {
+        float currChAvg=0, pivotChAvg=0;
+        chTemp = chN;
+        while (chTemp != 0) {
+            chTemp--;
+            currChAvg += arr[j+chTemp];
+            pivotChAvg += arr[r-chTemp];
+        }
+        currChAvg /= 3; pivotChAvg /= 3;
+
+        if (currChAvg < pivotChAvg) {
+            i+=chN;
+            
+            chTemp = chN;
+            while (chTemp != 0) {
+                chTemp--;
+                int temp = arr[i+chTemp];
+                arr[i+chTemp] = arr[j+chTemp];
+                arr[j+chTemp] = temp;
+            }
+        }
+    }
+    int pIdx = i+chN;
+
+    chTemp = chN;
+    while (chTemp != 0) {
+        chTemp--;
+        int temp = arr[pIdx+chTemp];
+        arr[pIdx+chTemp] = arr[r+chTemp];
+        arr[r+chTemp] = temp;
+    }
+    
+    quickSortPixels(arr, l, pIdx-chN, chN);
+    quickSortPixels(arr, pIdx+chN, r, chN);
+}
--- src/filters/pixelSort.c
+++ src/filters/pixelSort.c
@@ -2,53 +2,21 @@
 #include "filters_internal.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "../dimension_tools/dimension_tools.h"
 
 void horizontalPixelSort(char* filename) {
     IMAGE img = readImg(filename);
 
+    while (img.size > 1686000) { // number before stack overflow (on my pc) - 1000x562 rgb image
+        IMAGE newProps = { img.data, img.size, (img.width / 2), img.height, img.channelNumber };
+        printf("Image is too large, downsizing...\n");
+        nearestNeighborResize(filename, newProps); 
+        img = readImg("processedImg.jpg");
+    } 
+
     quickSortPixels(img.data, 0, img.size, img.channelNumber);
-    
+
     submitChanges(img);
     printf("Pixels sorted successfully.\n");
     free(img.data);
-}
-
-void quickSortPixels(uint8_t* arr, int l, int r, int chN) {
-    if (l >= r) return;
-    int i=l-chN, j=0, chTemp;
-    for (j=l; j<r; j+=chN) 
-    {
-        float currChAvg=0, pivotChAvg=0;
-        chTemp = chN;
-        while (chTemp != 0) {
-            chTemp--;
-            currChAvg += arr[j+chTemp];
-            pivotChAvg += arr[r-chTemp];
-        }
-        currChAvg /= 3; pivotChAvg /= 3;
-
-        if (currChAvg < pivotChAvg) {
-            i+=chN;
-            
-            chTemp = chN;
-            while (chTemp != 0) {
-                chTemp--;
-                int temp = arr[i+chTemp];
-                arr[i+chTemp] = arr[j+chTemp];
-                arr[j+chTemp] = temp;
-            }
-        }
-    }
-    int pIdx = i+chN;
-
-    chTemp = chN;
-    while (chTemp != 0) {
-        chTemp--;
-        int temp = arr[pIdx+chTemp];
-        arr[pIdx+chTemp] = arr[r+chTemp];
-        arr[r+chTemp] = temp;
-    }
-    
-    quickSortPixels(arr, l, pIdx-chN, chN);
-    quickSortPixels(arr, pIdx+chN, r, chN);
 }
